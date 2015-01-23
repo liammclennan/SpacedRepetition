12d9681c57602114dabc11b04bb2db254ff6d60c
Liam McLennan <liam.mclennan@gmail.com>
2015-01-23 20:07:49 +1000

    handle different server locations

--- src/Api/www/assets/js/server.js
+++ src/Api/www/assets/js/server.js
@@ -1,5 +1,5 @@
 ﻿define('server', ['fermat'], function (fermat) {
-    var appUrl = 'http://localhost:11285';
+    var appUrl = window.location.host.indexOf("localhost") > -1 ? 'http://localhost:11285' : 'http://studynotesapi.azurewebsites.net';
     var serverStates = fermat({
         Cards: appUrl + '/cards/:deckId',
         'Card/Result': appUrl + '/card/:cardId/result/:result'
